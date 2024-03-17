544f193824d97941748353a82af4df3fb4a88423
mtkennerly <mtkennerly@gmail.com>
2024-03-16 22:10:37 -0400

    Re-add PyInstaller

--- tasks.py
+++ tasks.py
@@ -1,19 +1,6 @@
 from invoke import task
-import shutil
 
 
 @task
 def build(context):
-    context.run("pyinstaller exesexe.py --onefile --specpath build --workpath build")
-
-
-@task
-def dist(context):
-    shutil.rmtree("./dist", ignore_errors=True)
-    context.run("python setup.py sdist")
-    context.run("python setup.py bdist_wheel")
-
-
-@task
-def test(context):
-    context.run("pytest tests")
+    context.run("poetry run pyinstaller exesexe.py --onefile --specpath build --workpath build")
